647bcf9d7dac6e95e735f4154f9cf3a2ca4d27a5
kingnigelpat <patricknigel33@gmail.com>
2026-03-02 23:33:23 +0100

    Include env.js for online deployment

---
+++ env.js
@@ -0,0 +1,11 @@
+// Firebase Configuration
+// Keep this file out of version control (listed in .gitignore)
+export const firebaseConfig = {
+    apiKey: "AIzaSyAqVeXjplLd9oT1KKGiq_J7L8IrWhx8Y68",
+    authDomain: "portal-cpt.firebaseapp.com",
+    projectId: "portal-cpt",
+    storageBucket: "portal-cpt.firebasestorage.app",
+    messagingSenderId: "996686724362",
+    appId: "1:996686724362:web:93def0c83fe9b225120ef3",
+    measurementId: "G-Y14QPSM5ZX"
+};
